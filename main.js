8c57b1cfd1bec517eb0cc19fe1665fdaa73ca1f8
Cassfsd <perezcasandra17@outlook.com>
2022-06-27 12:26:11 -0300

    update

---
+++ main.js
@@ -0,0 +1,8 @@
+console.log(nombreCompleto);/* CammelCase */
+let nombreCompleto ="Casandra Pérez"
+let numeroFavorito=9;
+const horasEnUnDia=24;
+console.log(horasEnUnDia)
+
+/* si no le pongo igua solo esta declarada y lo da como undefined
+ */
